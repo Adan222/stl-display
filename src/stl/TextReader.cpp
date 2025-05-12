4c060242a702474949614c2c5c9780387b69b19e
Adan222 <kamil.fugiel@s2innovation.com>
2025-05-12 18:58:57 +0200

    Implement ASCI STL file reader

--- src/stl/TextReader.cpp
+++ src/stl/TextReader.cpp
@@ -1,10 +1,67 @@
 #include "TextReader.hpp"
 
+/** STD */
+#include <cctype>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
+/** GLM */
+#include <glm/ext/vector_float3.hpp>
+
+/** LIB STL */
 #include "stl/Mesh.hpp"
 #include "stl/Reader.hpp"
 
+/** Private methods */
+
+std::string stl::TextReader::cleanLine(const std::string &line) const {
+    std::string s = line;
+
+    s.erase(0, s.find_first_not_of(" \n\r\t"));
+    s.erase(s.find_last_not_of(" \n\r\t") + 1);
+
+    return s;
+}
+
+std::vector<std::string> stl::TextReader::split(const std::string &str, const std::string &) const {
+    std::istringstream iss(str);
+    std::vector<std::string> tokens;
+    std::string token;
+    while (iss >> token) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+glm::vec3 stl::TextReader::readNormalVector(const std::vector<std::string> &line) const {
+    if (line[1] != "normal")
+        throw std::runtime_error("Unknown keyword: " + line[1]);
+
+    float x = std::stof(line[2]);
+    float y = std::stof(line[3]);
+    float z = std::stof(line[4]);
+
+    return glm::vec3(x, y, z);
+}
+
+glm::vec3 stl::TextReader::readVertex(const std::vector<std::string> &line) const {
+    try {
+        float x = std::stof(line[1]);
+        float y = std::stof(line[2]);
+        float z = std::stof(line[3]);
+        return glm::vec3(x, y, z);
+    } catch (const std::exception &e) {
+        std::cerr << "Failed to parse vertex: ";
+        for (const auto &part : line)
+            std::cerr << "[" << part << "] ";
+        std::cerr << "\nError: " << e.what() << std::endl;
+        throw;
+    }
+}
 /** Constructors */
 
 stl::TextReader::TextReader(std::fstream &&fstream) : stl::Reader(std::move(fstream)) {}
@@ -14,6 +71,58 @@ stl::TextReader::~TextReader() {}
 /** Public methods */
 
 stl::Mesh stl::TextReader::read() const {
-    std::cout << "TextReader\n";
-    return Mesh(std::vector<Triangle>());
+    unsigned int vertexIndex = 0;
+    std::vector<glm::vec3> vertices;
+
+    std::string rawLine;
+    for (std::string line; std::getline(_fstream, rawLine);) {
+        if (rawLine.empty())
+            continue;
+
+        const std::string processedLine = cleanLine(rawLine);
+        std::vector<std::string> lineList = split(processedLine, " ");
+
+        if (lineList.empty())
+            continue;
+
+        const std::string &cmd = lineList[0];
+
+        if (cmd == "solid") {
+        }
+
+        else if (cmd == "facet") {
+        }
+
+        else if (cmd == "outer" && lineList.size() > 1 && lineList[1] == "loop") {
+            vertexIndex = 0;
+        }
+
+        else if (cmd == "vertex") {
+            if (vertexIndex >= 3)
+                throw std::runtime_error("Too many vertices in facet.");
+
+            glm::vec3 v = readVertex(lineList);
+            vertices.emplace_back(v);
+            vertexIndex++;
+
+        }
+
+        else if (cmd == "endloop") {
+        }
+
+        else if (cmd == "endfacet") {
+            if (vertexIndex != 3) {
+                throw std::runtime_error("Incomplete triangle: vertex count = " +
+                                         std::to_string(vertexIndex));
+            }
+        }
+
+        else if (cmd == "endsolid")
+            return vertices;
+
+        else
+            throw std::runtime_error("Unknown keyword: " + cmd);
+    }
+
+    throw std::runtime_error("Missing endsolid statement in STL file.");
 }
--- src/stl/TextReader.hpp
+++ src/stl/TextReader.hpp
@@ -1,15 +1,43 @@
 #pragma once
 
+/** STD */
+#include <vector>
+
+/** GLM */
+#include <glm/ext/vector_float3.hpp>
+
+/** LIB STL */
 #include "stl/Reader.hpp"
 
 namespace stl {
 
 class TextReader : public stl::Reader {
+  private:
+    /**
+     * Clean line from white spaces
+     */
+    std::string cleanLine(const std::string &line) const;
+
+    /**
+     * Split line int array of strings
+     */
+    std::vector<std::string> split(const std::string &str, const std::string &delimeter) const;
+
+    /**
+     * Read line and get normal vector of the triangle
+     */
+    glm::vec3 readNormalVector(const std::vector<std::string> &line) const;
+
+    /**
+     * Read line and return vector for a vertex
+     */
+    glm::vec3 readVertex(const std::vector<std::string> &line) const;
+
   public:
     TextReader(std::fstream &&fstream);
     ~TextReader();
 
-    stl::Mesh read() const;
+    stl::Mesh read() const override;
 };
 
 } // namespace stl
