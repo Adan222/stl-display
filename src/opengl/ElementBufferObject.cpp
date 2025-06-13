f5be88800c41b1a065267a04070f84939e30ec1e
Adan222 <kamil.fugiel@s2innovation.com>
2025-06-13 23:12:49 +0200

    Clean code for ElementBufferObject class

--- src/opengl/ElementBufferObject.cpp
+++ src/opengl/ElementBufferObject.cpp
@@ -1,12 +1,14 @@
 #include "ElementBufferObject.hpp"
 
-#include <cstddef>
+/** Constructors */
 
-ElementBufferObject::ElementBufferObject() { glGenBuffers(1, &this->_id); }
+ElementBufferObject::ElementBufferObject() { glGenBuffers(1, &_id); }
 
 ElementBufferObject::~ElementBufferObject() {}
 
+/** Public methods */
+
 void ElementBufferObject::setData(const void *data, std::size_t size) {
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->_id);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _id);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
 }
--- src/opengl/ElementBufferObject.hpp
+++ src/opengl/ElementBufferObject.hpp
@@ -1,9 +1,14 @@
 #pragma once
 
+/** STD */
 #include <cstddef>
 
+/** GLAD */
 #include <glad/glad.h>
 
+/**
+ * Class that represents Element Buffer Object in OpenGL
+ */
 class ElementBufferObject {
   private:
     GLuint _id;
@@ -12,5 +17,8 @@ class ElementBufferObject {
     ElementBufferObject();
     ~ElementBufferObject();
 
+    /**
+     * Set indices data for Element Buffer Object
+     */
     void setData(const void *data, std::size_t size);
 };
